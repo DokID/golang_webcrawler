3cf510b5528ea319d72b53e2ca8c4803c8c912b8
Niklas Bergdahl <meradana@hotmail.se>
2016-05-16 11:02:08 +0200

    Added skeleton code, need review
    Will slices work for our purposes? Should we have the controller be in the
    main function or a different class entirely? Do we want more functions?

---
+++ code/Crawler.go
@@ -0,0 +1,27 @@
+// Gábor Nagy and Niklas Ingemar Bergdahl 2016-05-16
+
+package main
+
+import (
+  "fmt"
+  "bufio"
+)
+
+// A variable containing the visited URLs
+var visited []string = {
+
+}
+
+// A variable containing the visited URLs
+var toVisit []string = {
+}
+
+// The main function, starts the crawler on a
+// given URL. TODO: add print to a file for all
+// visited URLs.
+func main() {
+}
+
+// Crawls the given URL looking for more URLs
+func crawl(URL string) {
+}
