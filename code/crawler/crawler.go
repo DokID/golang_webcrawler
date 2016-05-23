578fb8f5b0e1c62a1b61c68763a2d06a14859cdc
gaborn <gaborn@kth.se>
2016-05-23 17:01:13 +0200

    Implemented webpage parsing and hyperlink recognition
    The code now parses the received page with the help of the package "golang.org/x/net/html", and scrapes links starting with "/wiki/". It is, however unstable, and panics with "invalid memory address or nil pointer dereference" after a num
    ber of iterations.

--- code/crawler/crawler.go
+++ code/crawler/crawler.go
@@ -7,27 +7,76 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 // Crawl calls and parses the assigned webpage, looking for URLs and collecting
 // them in a list.
-func Crawl(url string) *list.List {
+func Crawl(url string) (list *list.List) {
 	body := getPage(url)
-	log.Print(body)
-	return &list.List{}
+	defer body.Close()
+	tokenizer := html.NewTokenizer(body)
+	log.Println("Tokenizer created!")
+
+	for {
+		tt := tokenizer.Next()
+
+		log.Printf("%v", tt)
+
+		switch tt {
+		case html.ErrorToken:
+			if tokenizer.Err() != io.EOF {
+				log.Printf("Token Error: %v", tokenizer.Err())
+			}
+			log.Println("Error: EOF")
+			return
+		case html.StartTagToken:
+			token := tokenizer.Token()
+			if token.Data != "a" {
+				continue
+			}
+
+			log.Println("Token found!")
+
+			found, href := getHref(token)
+			if !found {
+				continue
+			}
+
+			if strings.Index(href, "/wiki/") == 0 {
+				list.PushFront(href)
+				log.Println("URL located!")
+			}
+
+		}
+	}
 }
 
 // Helper function used to retrieve the contents of the specified webpage.
-func getPage(url string) (body *io.ReadCloser) {
+func getPage(url string) (body io.ReadCloser) {
 	// Retrieve the page
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		log.Fatalf("HTTP Error: %v", err)
 	}
-	defer resp.Body.Close()
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		log.Fatalf("Response Error: %v", err)
 	}
-	body = &resp.Body
+	body = resp.Body
+	log.Println("Body received!")
+	return
+}
+
+// Helper function used to identify href:s in an html file
+func getHref(t html.Token) (found bool, href string) {
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			found = true
+			log.Println("Href found!")
+		}
+	}
 	return
 }
