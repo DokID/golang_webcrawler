215e2b19f473c8370891678fd7a63da631b4e164
gaborn <gaborn@kth.se>
2016-05-25 00:43:42 +0200

    Added extra waitgroup
    There was a problem with the first thread taking longer to execute than the innermost loop of the main function, resulting in the end condition being met and the program exiting after just the first URL

--- code/Main.go
+++ code/Main.go
@@ -5,10 +5,11 @@ package main
 import (
 	"fmt"
 	"inda-project/code/crawler"
+	"runtime"
 	"sync"
 )
 
-// A hash map containing the visited URLs
+// A hash map containing the visited URL:s
 var visited = make(map[string]bool)
 
 // A hash map containing the URLs to be visited
@@ -19,29 +20,40 @@ var startURL = "http://en.wikipedia.org/wiki/Main_Page"
 
 // The main function, starts the crawler on a
 // given URL. TODO: add print to a file for all
-// visited URLs.
+// visited URL:s.
 func main() {
 	toVisit[startURL] = true
-	var wg sync.WaitGroup
+	wg1, wg2 := new(sync.WaitGroup), new(sync.WaitGroup)
+	mutex := new(sync.Mutex)
 
+	// The main for-loop responsible for sending crawlers to all known pages
 	for {
 		for urlkey, exists := range toVisit {
-			wg.Add(1)
+			wg1.Add(1)
+			wg2.Add(1)
+			// A crawler thread starts
 			go func(urlkey string, exists bool) {
 				if exists {
+					mutex.Lock()
 					delete(toVisit, urlkey)
 					visited[urlkey] = true
 					tMap := crawler.Crawl(urlkey)
 					controller(tMap)
+					mutex.Unlock()
+					runtime.Gosched()
 				}
-				wg.Done()
+				wg1.Done()
+				wg2.Done()
 			}(urlkey, exists)
 		}
+		wg1.Wait()
+		// If no more pages left to visit, break
 		if len(toVisit) == 0 {
 			break
 		}
 	}
-	wg.Wait()
+	wg2.Wait()
+	// When done crawling, print found URL:s
 	for urlkey, exists := range visited {
 		if exists {
 			fmt.Println(urlkey)
@@ -49,9 +61,11 @@ func main() {
 	}
 }
 
-func controller(m map[string]bool) {
-	for i, _ := range m {
-		if toVisit[i] == false && visited[i] == false {
+// Helper function responsible for adding newly acquired
+// unvisited pages to the queue.
+func controller(tMap map[string]bool) {
+	for i := range tMap {
+		if visited[i] == false {
 			toVisit[i] = true
 		}
 	}
