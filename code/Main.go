1840d7ddc5d90e38b4fca589a657c0bc03f5e9c0
gaborn <gaborn@kth.se>
2016-05-31 19:04:27 +0200

    Added print to file feature and optimized runtime
    The program now prints visited pages in a specially formatted CSV file. I have also made a number of optimizations.

--- code/Main.go
+++ code/Main.go
@@ -5,6 +5,8 @@ package main
 import (
 	"fmt"
 	"inda-project/code/crawler"
+	"log"
+	"os"
 	"runtime"
 	"sync"
 	"time"
@@ -30,50 +32,70 @@ var startURL = "http://en.wikipedia.org/wiki/Main_Page"
 func main() {
 	lib.toVisit[startURL] = true
 	wg := new(sync.WaitGroup)
+	ch := make(chan string, 200)
+
+	ch <- startURL
+
+	// Start filewriter
+	go save(ch)
 
 	// The main for-loop responsible for sending crawlers to all known pages
 	for {
-		for urlkey, exists := range lib.toVisit {
+		for urlkey := range ch {
 			// A crawler thread starts
-			fmt.Println(runtime.NumGoroutine())
 			for runtime.NumGoroutine() >= 200 {
-				time.Sleep(10 * time.Millisecond)
+				time.Sleep(20 * time.Millisecond)
 			}
 			wg.Add(1)
 			// A crawler thread starts
-			go func(urlkey string, exists bool) {
-				if exists {
-					lib.Lock()
-					delete(lib.toVisit, urlkey)
-					lib.visited[urlkey] = true
-					tMap := crawler.Crawl(urlkey)
-					controller(tMap)
-					lib.Unlock()
-					runtime.Gosched()
-				}
+			go func(urlkey string) {
+				lib.Lock()
+				delete(lib.toVisit, urlkey)
+				lib.visited[urlkey] = true
+				tMap := crawler.Crawl(urlkey)
+				controller(tMap, ch)
+				fmt.Println(len(lib.visited))
+				lib.Unlock()
+				runtime.Gosched()
 				wg.Done()
-			}(urlkey, exists)
+			}(urlkey)
 		}
 		wg.Wait()
 		// If no more pages left to visit, break
-		if len(lib.toVisit) == 0 {
+		if len(lib.toVisit) == 0 || len(lib.visited) >= 500000 {
+			close(ch)
 			break
-		}
-	}
-	// When done crawling, print found URL:s
-	for urlkey, exists := range lib.visited {
-		if exists {
-			fmt.Println(urlkey)
 		}
 	}
 }
 
 // Helper function responsible for adding newly acquired
 // unvisited pages to the queue.
-func controller(tMap map[string]bool) {
+func controller(tMap map[string]bool, ch chan<- string) {
 	for i := range tMap {
 		if lib.visited[i] == false {
 			lib.toVisit[i] = true
+			ch <- i
+		}
+	}
+}
+
+// Helper function used to flush the contents of the visited map to a
+// CSV file.
+func save(ch <-chan string) {
+	file, err := os.Create("nodes.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	_, err = file.WriteString("pageId:ID,name,:LABEL\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for key := range ch {
+		_, err := file.WriteString(key + "," + key + ",WEBPAGE\n")
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
 }
